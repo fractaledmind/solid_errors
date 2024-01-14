d648e355567e7c49c6a07388732193383d255bfa
Stephen Margheim <stephen.margheim@gmail.com>
2024-01-15 00:20:48 +0100

    Require the sanitizer

--- lib/solid_errors.rb
+++ lib/solid_errors.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "solid_errors/version"
+require_relative "solid_errors/sanitizer"
 require_relative "solid_errors/subscriber"
 require_relative "solid_errors/backtrace"
 require_relative "solid_errors/backtrace_line"
