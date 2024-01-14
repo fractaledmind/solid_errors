f6e750c01e5398b1a09a27c4fe07544231826e7d
Stephen Margheim <stephen.margheim@gmail.com>
2024-01-15 00:46:40 +0100

    Fix generator to accept database option

--- lib/generators/solid_errors/install/install_generator.rb
+++ lib/generators/solid_errors/install/install_generator.rb
@@ -1,13 +1,32 @@
 # frozen_string_literal: true
 
-class SolidErrors::InstallGenerator < Rails::Generators::Base
-  source_root File.expand_path("templates", __dir__)
+require 'rails/generators'
+require 'rails/generators/active_record'
 
-  class_option :skip_migrations, type: :boolean, default: nil, desc: "Skip migrations"
+module SolidErrors
+  #
+  # Rails generator used for setting up SolidErrors in a Rails application.
+  # Run it with +bin/rails g solid_errors:install+ in your console.
+  #
+  class InstallGenerator < Rails::Generators::Base
+    include ActiveRecord::Generators::Migration
 
-  def create_migrations
-    unless options[:skip_migrations]
-      rails_command "railties:install:migrations FROM=solid_errors", inline: true
+    source_root File.expand_path("templates", __dir__)
+
+    class_option :database, type: :string, aliases: %i(--db), desc: "The database for your migration. By default, the current environment's primary database is used."
+    class_option :skip_migrations, type: :boolean, default: nil, desc: "Skip migrations"
+
+    # Generates monolithic migration file that contains all database changes.
+    def create_migration_file
+      return if options[:skip_migrations]
+
+      migration_template 'create_solid_errors_tables.rb.erb', File.join(db_migrate_path, "create_solid_errors_tables.rb")
+    end
+
+    private
+
+    def migration_version
+      "[#{ActiveRecord::VERSION::STRING.to_f}]"
     end
   end
 end
