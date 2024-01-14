06b729e7baeb57d8d8f471be8ad3398dba9aecd5
Stephen Margheim <stephen.margheim@gmail.com>
2024-01-14 23:46:52 +0100

    Remove stupid failing test

--- test/test_solid_errors.rb
+++ test/test_solid_errors.rb
@@ -6,8 +6,4 @@ class TestSolidErrors < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::SolidErrors::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
