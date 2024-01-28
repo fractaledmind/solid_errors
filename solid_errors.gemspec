d058bde1dc617393c2b735a94f778a9f10305149
Stephen Margheim <stephen.margheim@gmail.com>
2024-01-28 10:45:13 +0100

    Add internal test app to test the Rails engine

--- solid_errors.gemspec
+++ solid_errors.gemspec
@@ -22,6 +22,7 @@ Gem::Specification.new do |spec|
 
   # Uncomment to register a new dependency of your gem
   spec.add_dependency "rails", "~> 7.0"
+  spec.add_development_dependency "sqlite3"
 
   # For more information and examples about making a new gem, check out our
   # guide at: https://bundler.io/guides/creating_gem.html
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
+ENV["RAILS_ENV"] = "test"
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+
+require_relative "../test/dummy/config/environment"
+ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/migrate", __dir__)]
+require "rails/test_help"
 require "solid_errors"
 
 require "minitest/autorun"
