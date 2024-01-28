58737922d44b922db9444942d775c44dde89da27
Stephen Margheim <stephen.margheim@gmail.com>
2024-01-28 12:27:18 +0100

    Add basic auth to secure the dashboard

--- app/controllers/solid_errors/application_controller.rb
+++ app/controllers/solid_errors/application_controller.rb
@@ -1,4 +1,7 @@
 module SolidErrors
   class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    http_basic_authenticate_with name: SolidErrors.username, password: SolidErrors.password if SolidErrors.password
   end
 end
--- lib/solid_errors.rb
+++ lib/solid_errors.rb
@@ -7,4 +7,20 @@ require_relative "solid_errors/engine"
 
 module SolidErrors
   mattr_accessor :connects_to
+  mattr_accessor :username
+  mattr_accessor :password
+
+  class << self
+    # use method instead of attr_accessor to ensure
+    # this works if variable set after SolidErrors is loaded
+    def username
+      @username ||= ENV["SOLIDERRORS_USERNAME"]
+    end
+
+    # use method instead of attr_accessor to ensure
+    # this works if variable set after SolidErrors is loaded
+    def password
+      @password ||= ENV["SOLIDERRORS_PASSWORD"]
+    end
+  end
 end
