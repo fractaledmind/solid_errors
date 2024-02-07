d4157c0acf4551dba4d4d24ecc906c6f9aaa08cd
Stephen Margheim <stephen.margheim@gmail.com>
2024-02-07 19:00:14 +0100

    Patch the action_dispatch executor to report rendered exceptions

--- lib/solid_errors/engine.rb
+++ lib/solid_errors/engine.rb
@@ -15,5 +15,20 @@ module SolidErrors
     initializer "solid_errors.active_record.error_subscriber" do
       Rails.error.subscribe(SolidErrors::Subscriber.new)
     end
+
+    initializer "solid_errors.active_support.notification_subscriber" do
+      ActiveSupport.on_load(:action_dispatch) do
+        class ActionDispatch::Executor
+          def call(env)
+            returned = super
+
+            if (exception = env['action_dispatch.exception'])
+              @executor.error_reporter.report(exception, handled: false, source: "application.action_dispatch")
+            end
+            returned
+          end
+        end
+      end
+    end
   end
 end
