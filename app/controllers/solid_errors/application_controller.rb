23f50349d84027498bf97cc5c7d460887f2aed42
Stephen Margheim <stephen.margheim@gmail.com>
2024-04-09 11:46:56 +0200

    Paginate occurrences

--- app/controllers/solid_errors/application_controller.rb
+++ app/controllers/solid_errors/application_controller.rb
@@ -3,5 +3,25 @@ module SolidErrors
     protect_from_forgery with: :exception
 
     http_basic_authenticate_with name: SolidErrors.username, password: SolidErrors.password if SolidErrors.password
+
+    # adapted from: https://github.com/ddnexus/pagy/blob/master/gem/lib/pagy.rb
+    OverflowError = Class.new(StandardError)
+    class Page
+      attr_reader :count, :items, :pages, :first, :last, :prev, :next, :offset, :from, :to
+
+      def initialize(collection, params)
+        @count = collection.count
+        @items = (params[:items] || 20).to_i
+        @pages = [(@count.to_f / @items).ceil, 1].max
+        @page = (page = (params[:page] || 1).to_i) > @pages ? @pages : page
+        @first = (1 unless @page == 1)
+        @last = (@pages unless @page == @pages)
+        @prev = (@page - 1 unless @page == 1)
+        @next = @page == @pages ? nil : @page + 1
+        @offset = (@items * (@page - 1))
+        @from = [@offset + 1, @count].min
+        @to   = [@offset + @items, @count].min
+      end
+    end
   end
 end
