b2e1ded975fb6ca842190c00f8d6563a0c037418
Craig Kaminsky <imageaid@gmail.com>
2024-07-18 10:56:06 -0600

    Adjustments from feedback on PR
    - Converted prev_resolved_at to a datetime from a timestamp in migration
    - Added a method `should_send_email?` to determine if an email goes instead of a string of conditionals
    - Cleaned up guard clauses
    - Improved the datetime comparison in an AR query

--- app/models/solid_errors/occurrence.rb
+++ app/models/solid_errors/occurrence.rb
@@ -2,7 +2,7 @@ module SolidErrors
   class Occurrence < Record
     belongs_to :error, class_name: "SolidErrors::Error"
 
-    after_create_commit :send_email, if: -> { SolidErrors.send_emails? && SolidErrors.email_to.present? && under_limit? }
+    after_create_commit :send_email, if: :should_send_email?
 
     # The parsed exception backtrace. Lines in this backtrace that are from installed gems
     # have the base path for gem installs replaced by "[GEM_ROOT]", while those in the project
@@ -24,12 +24,15 @@ module SolidErrors
       ErrorMailer.error_occurred(self).deliver_later
     end
 
-    def under_limit?
-      return true if error.occurrences.count == 1 || !SolidErrors.one_email_per_occurrence
+    def should_send_email?
+      SolidErrors.send_emails? && SolidErrors.email_to.present? && under_limit?
+    end
 
-      error.occurrences.where("created_at > ?", error.prev_resolved_at).count == 1
-    rescue
-      true
+    def under_limit?
+      return true if error.occurrences.count == 1
+      return true if !SolidErrors.one_email_per_occurrence
+
+      error.occurrences.where(created_at: error.prev_resolved_at..).one?
     end
   end
 end
