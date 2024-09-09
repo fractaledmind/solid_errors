4ff64874c93371c703b00361ec2b9645dd71c22f
Stephen Margheim <stephen.margheim@gmail.com>
2024-09-09 21:12:25 +0200

    Update installer to configure Solid Errors in production

--- lib/generators/solid_errors/install/install_generator.rb
+++ lib/generators/solid_errors/install/install_generator.rb
@@ -11,5 +11,20 @@ module SolidErrors
     def add_solid_errors_db_schema
       template "db/errors_schema.rb"
     end
+
+    def configure_solid_errors
+      insert_into_file Pathname(destination_root).join("config/environments/production.rb"), after: /^([ \t]*).*?(?=\nend)$/ do
+        [
+          "",
+          '\1# Configure Solid Errors',
+          '\1config.solid_errors.connects_to = { database: { writing: :errors } }',
+          '\1config.solid_errors.send_emails = true',
+          '\1config.solid_errors.email_from = ""',
+          '\1config.solid_errors.email_to = ""',
+          '\1config.solid_errors.username = Rails.application.credentials.dig(:solid_errors, :username)',
+          '\1config.solid_errors.password = Rails.application.credentials.dig(:solid_errors, :password)',
+        ].join("\n")
+      end
+    end
   end
 end
