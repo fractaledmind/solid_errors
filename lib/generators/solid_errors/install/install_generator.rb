d09251d0a25bd64329a32c2a8da97e34439005e0
Stephen Margheim <stephen.margheim@gmail.com>
2024-09-19 01:53:25 +0300

    Fix standard issue

--- lib/generators/solid_errors/install/install_generator.rb
+++ lib/generators/solid_errors/install/install_generator.rb
@@ -22,7 +22,7 @@ module SolidErrors
           '\1config.solid_errors.email_from = ""',
           '\1config.solid_errors.email_to = ""',
           '\1config.solid_errors.username = Rails.application.credentials.dig(:solid_errors, :username)',
-          '\1config.solid_errors.password = Rails.application.credentials.dig(:solid_errors, :password)',
+          '\1config.solid_errors.password = Rails.application.credentials.dig(:solid_errors, :password)'
         ].join("\n")
       end
     end
