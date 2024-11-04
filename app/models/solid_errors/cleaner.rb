fefb8873d56c2990766b3c534f9d9a7a7ef44e50
Ron Shinall <81988008+ron-shinall@users.noreply.github.com>
2024-11-04 10:02:24 -0700

    Update indentation

--- app/models/solid_errors/cleaner.rb
+++ app/models/solid_errors/cleaner.rb
@@ -6,30 +6,31 @@ module SolidErrors
       end
 
       private
-        def destroy_after_last_create?
-          SolidErrors.destroy_after.respond_to?(:ago) && (SolidErrors::Occurrence.last.id % 100).zero?
-        end
 
-        def destroy_records
-          ActiveRecord::Base.transaction do
-            destroy_occurrences
-            destroy_errors
-          end
-        end
+      def destroy_after_last_create?
+        SolidErrors.destroy_after.respond_to?(:ago) && (SolidErrors::Occurrence.last.id % 100).zero?
+      end
 
-        def destroy_occurrences
-          SolidErrors::Occurrence.joins(:error)
-                                 .merge(SolidErrors::Error.resolved)
-                                 .where(created_at: ...SolidErrors.destroy_after.ago)
-                                 .delete_all
+      def destroy_records
+        ActiveRecord::Base.transaction do
+          destroy_occurrences
+          destroy_errors
         end
+      end
 
-        def destroy_errors
-          SolidErrors::Error.resolved
-                            .where
-                            .missing(:occurrences)
-                            .delete_all
-        end
+      def destroy_occurrences
+        SolidErrors::Occurrence.joins(:error)
+                                .merge(SolidErrors::Error.resolved)
+                                .where(created_at: ...SolidErrors.destroy_after.ago)
+                                .delete_all
+      end
+
+      def destroy_errors
+        SolidErrors::Error.resolved
+                          .where
+                          .missing(:occurrences)
+                          .delete_all
+      end
     end
   end
 end
