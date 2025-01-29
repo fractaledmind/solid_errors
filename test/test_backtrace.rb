637984cc1143c7209abe2160e3a122f585d29c32
Ohkubo KOHEI <o@kbn.one>
2025-01-29 00:14:13 +0000

    fix error

--- test/test_backtrace.rb
+++ test/test_backtrace.rb
@@ -5,6 +5,6 @@ require "test_helper"
 class TestSolidErrors < Minitest::Test
   def test_backtrace
     backtrace = SolidErrors::Backtrace.parse(caller(0)).to_a
-    assert_equal backtrace[0][:method], "TestSolidErrors#test_backtrace"
+    assert_includes backtrace[0][:method], "test_backtrace"
   end
 end
